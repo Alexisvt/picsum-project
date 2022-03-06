e818642dfa0f8b59947816225ded2e5b6e94dc33
Alexis Villegas Torres <alexisvt@gmail.com>
2022-03-06 12:59:59 -0600

    Remove placeholder URL and read it just from env

--- src/app/services/baseAPIConfig.ts
+++ src/app/services/baseAPIConfig.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseAPIConfig = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_BASE_URL || 'https://picsum.photos/',
+    baseUrl: process.env.REACT_APP_API_BASE_URL,
   }),
   endpoints: () => ({}),
 });
