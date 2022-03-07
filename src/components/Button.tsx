4c32f794313608f9c923de6e204b22597347fe68
Alexis Villegas Torres <alexisvt@gmail.com>
2022-03-06 18:23:40 -0600

    Change button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,16 +3,17 @@ import styled from 'styled-components';
 const Button = styled.button`
   width: 6.25rem;
   border: none;
-  color: #000;
+  color: #fff;
   font-size: 1.25rem;
   font-weight: bold;
   padding: 0.5rem 1rem;
   border-radius: 0.25rem;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
+  background: #0f2f97;
   &:hover {
     background: #5074eb;
-    color: white;
+    color: #fff;
   }
 `;
 
